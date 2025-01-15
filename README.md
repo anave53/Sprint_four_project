020122161b8222198bcf64fbe5a72dbeccae24e7
Alex Nave <anave97@outlook.com>
2025-01-15 13:31:03 -0800

    Submitting to Streamlit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sprint-4-Project
+# This project involves taking a dataset, performing basic analysis, creating visualizations of the dataset, and then uploading through render.
+# For this project, I will be using a dataset that shows information about various cars listed for sale. 
---
+++ app.py
@@ -0,0 +1,33 @@
+import plotly.express as px
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Read the dataset and remove missing values
+vehicles_df = pd.read_csv('vehicles_us.csv')
+vehicles_df.dropna()
+
+#Remove outliers
+Q1 = vehicles_df[numeric_cols].quantile(0.25)
+Q3 = vehicles_df[numeric_cols].quantile(0.75)
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+
+vehicles_processed = vehicles_df[~((vehicles_df[numeric_cols] < lower_bound) | (vehicles_df[numeric_cols] > upper_bound)).any(axis=1)]
+
+st.header('Car Listing Analysis')
+st.write('Comparing the listings of old cars vs new cars')
+#Plot the car age vs price in a scatterplot
+fig_one = px.scatter(vehicles_processed, x='model_year', y='price', title='Car Age vs. Price')
+st.plotly_chart(fig_one)
+
+
+#Plot the distribution of car prices in a histogram
+fig_two = px.histogram(vehicles_processed, x='price', title='Distribution of Car Prices')
+st.plotly_chart(fig_two)
+
+
